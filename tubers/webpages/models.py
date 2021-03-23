586b6f4160beb135b06520d985c96e8f19c4e4c4
ankus <iamankus7@gmail.com>
2021-03-24 02:51:45 +0530

    assignment 2

--- tubers/webpages/models.py
+++ tubers/webpages/models.py
@@ -26,6 +26,7 @@ class Team(models.Model):
     insta_link = models.CharField(max_length=255)
     photo = models.ImageField(upload_to='media/team/%Y/%m/%d/')
     created_date = models.DateTimeField(auto_now_add=True)
+    youtube_link=models.CharField(max_length=255,blank=True)
     
     def __str__(self):
         return self.first_name
