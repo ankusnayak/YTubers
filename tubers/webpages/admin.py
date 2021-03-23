3e07cda5a2ff65b2209741fd5f39ab0c645510b3
ankus <iamankus7@gmail.com>
2021-03-24 02:35:17 +0530

    end of module 5

--- tubers/webpages/admin.py
+++ tubers/webpages/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from .models import Slider
+from .models import Slider, Team
+from django.utils.html import format_html
 # from . import models
 
+#this teamAdmin by convention is used for display the property of the Team
+class TeamAdmin(admin.ModelAdmin):
+
+    
+    def myphoto(self, object):
+        return format_html('<img src="{}" width="40"/>'.format(object.photo.url))
+
+    list_display = ('id','myphoto','first_name', 'role', 'created_date')
+    list_display_links = ('first_name', 'id')
+    search_fields = ('first_name', 'role')
+    list_filter=('role',)
 # Register your models here.
 admin.site.register(Slider)
+admin.site.register(Team,TeamAdmin)
--- tubers/webpages/views.py
+++ tubers/webpages/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Slider
+from .models import Slider,Team
 
 # Create your views here.
 #these are all method based views
@@ -7,9 +7,11 @@ def home(request):
     # get the data from database
     # consume all the slider objects present in the database 
     sliders = Slider.objects.all()
+    teams=Team.objects.all()
     # now pass the array or list of object as a object form to the template 
     data = {
-        'sliders':sliders
+        'sliders': sliders,
+        'teams':teams
     } 
     return render(request,'webpages/home.html',data)
 def about(request):
