f7ef143d27628ffc08239f15b955e0d385714f30
ankus <iamankus7@gmail.com>
2021-03-25 18:48:59 +0530

    complete all template stuff

--- tubers/youtubers/views.py
+++ tubers/youtubers/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-
+from .models import Youtuber
 # Create your views here.
 def youtubers(request):
-    pass
+    #get all the tubers not featured_wise
+    tubers = Youtuber.objects.order_by('-created_date')
+    data = {
+        'tubers':tubers,
+    }
+    return render(request,'youtubers/youtubers.html',data)
 def youtubers_detail(request,id):
-    pass
+    tuber = get_object_or_404(Youtuber, pk=id)
+    data = {
+        'tuber':tuber,
+    }
+    return render(request,'youtubers/youtubers_detail.html',data)
 def search(request):
     pass
